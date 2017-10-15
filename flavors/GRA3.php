6e24fc11856f3d19053c7ce9e0ba8cb2e60da4c4
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-15 22:20:44 +0000

    Update GRA3.php

--- flavors/GRA3.php
+++ flavors/GRA3.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json');
 $json_array = array(
-    "FLGRA3" => array(
-      "S1" => array(
+    "FLAVOR" => array(
+      "GRA3-S1" => array(
             "[outboundBandwidth]" => "102",
             "[disk]" => "10",
             "[region]" => "GRA3",
@@ -15,7 +15,7 @@ $json_array = array(
             "[available]" => "1",
             "[ram]" => "2000"
         ),
-      "S2" => array(
+      "GRA3-S2" => array(
             "[outboundBandwidth]" => "102",
             "[disk]" => "20",
             "[region]" => "GRA3",
@@ -28,13 +28,52 @@ $json_array = array(
             "[available]" => "1",
             "[ram]" => "4000"
         ),
-      "S3" => array(
+      "GRA3-S3" => array(
             "[outboundBandwidth]" => "102",
             "[disk]" => "40",
             "[region]" => "GRA3",
             "[name]" => "s1-8",
             "[inboundBandwidth]" => "102",
             "[id]" => "021668a3-623a-40ef-b15d-ba2a69e74a2f",
+            "[vcpus]" => "2",
+            "[type]" => "ovh.vps-ssd",
+            "[osType]" => "linux",
+            "[available]" => "1",
+            "[ram]" => "8000"
+        ),
+      "SBG3-S1" => array(
+            "[outboundBandwidth]" => "102",
+            "[disk]" => "10",
+            "[region]" => "SBG3",
+            "[name]" => "s1-2",
+            "[inboundBandwidth]" => "102",
+            "[id]" => "35e3c9b8-655f-4622-b34b-8df60cf099d5",
+            "[vcpus]" => "1",
+            "[type]" => "ovh.vps-ssd",
+            "[osType]" => "linux",
+            "[available]" => "1",
+            "[ram]" => "2000"
+        ),
+      "SBG3-S2" => array(
+            "[outboundBandwidth]" => "102",
+            "[disk]" => "20",
+            "[region]" => "SBG3",
+            "[name]" => "s1-4",
+            "[inboundBandwidth]" => "102",
+            "[id]" => "ac07fd3f-147a-4453-a4ae-851f02c38495",
+            "[vcpus]" => "1",
+            "[type]" => "ovh.vps-ssd",
+            "[osType]" => "linux",
+            "[available]" => "1",
+            "[ram]" => "4000"
+        ),
+      "SBG3-S3" => array(
+            "[outboundBandwidth]" => "102",
+            "[disk]" => "40",
+            "[region]" => "SBG3",
+            "[name]" => "s1-8",
+            "[inboundBandwidth]" => "102",
+            "[id]" => "97af8010-e4bf-4201-8860-33bacd514f99",
             "[vcpus]" => "2",
             "[type]" => "ovh.vps-ssd",
             "[osType]" => "linux",

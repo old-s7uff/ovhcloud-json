bdf6432b81c89b2c2bba4b5309a22ebfca0e6649
ƬHE ЯAW ☣ <raw@dopehosting.net>
2017-12-25 21:23:36 +0100

    Update mount.sh

--- backups/storage/mount.sh
+++ backups/storage/mount.sh
@@ -6,5 +6,7 @@ export OS_TENANT_NAME=""
 export OS_TENANT_ID=""
 export OS_REGION_NAME="GRA3"
 
-sudo mount -t svfs -o username=$OS_USERNAME,password=$OS_PASSWORD,tenant=$OS_TENANT_NAME,region=$OS_REGION_NAME pcs /backup
-echo "done"
+sudo mount -t svfs -o username=$OS_USERNAME,password=$OS_PASSWORD,tenant=$OS_TENANT_NAME,region=$OS_REGION_NAME pcs /raw
+clear
+echo "Mount Done"
+sleep 3

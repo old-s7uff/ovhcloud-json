f4970c8c93eaae69d58fd4411e1bba0e7fd9dbab
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-01-11 20:48:50 +0100

    Update rsync.sh

--- backups/storage/rsync.sh
+++ backups/storage/rsync.sh
@@ -2,4 +2,4 @@
 
 . mount.sh
 
-rsync --partial --stats --progress --no-perms /kvm/images/$1.qcow2 /backup/*/$1.qcow2 --temp-dir=/tmp/
+rsync --partial --stats --progress --no-perms /kvm/images/$1.qcow2 /backup/$CONTAINER/$1.qcow2 --temp-dir=/tmp/
